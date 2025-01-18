c5b569216a45bcd6c7648a752c8af4da97c0b4f8
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-18 23:16:40 +0100

    Create GetFieldsApi.php

---
+++ nocode/lib/KLXM/nocode/GetFieldsApi.php
@@ -0,0 +1,29 @@
+<?php
+namespace KLXM\nocode;
+
+class GetFieldsApi extends \rex_api_function
+{
+    protected $published = true;
+
+    function execute()
+    {
+        $tableName = rex_request('table', 'string');
+        
+        if (!$tableName) {
+            throw new \rex_api_exception('Keine Tabelle angegeben');
+        }
+
+        $mapper = new YFormMapper();
+        
+        try {
+            $fields = $mapper->getTableFields($tableName);
+            
+            return \rex_api_result::factory(true, [
+                'fields' => $fields
+            ]);
+            
+        } catch (\Exception $e) {
+            throw new \rex_api_exception($e->getMessage());
+        }
+    }
+}
