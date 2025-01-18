1d905e69ce460360d0cff43a7ff6c02650d34360
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-18 23:58:40 +0100

    Update GetFieldsApi.php

--- lib/api/GetFieldsApi.php
+++ lib/api/GetFieldsApi.php
@@ -1,29 +1,28 @@
 <?php
-namespace KLXM\nocode;
 
-class NoCodeGetFields extends \rex_api_function
+class rex_api_nocode_get_fields extends rex_api_function
 {
     protected $published = true;
 
     function execute()
     {
-        $tableName = \rex_request('table', 'string');
+        $tableName = rex_request('table', 'string');
         
         if (!$tableName) {
-            throw new \rex_api_exception('Keine Tabelle angegeben');
+            throw new rex_api_exception('Keine Tabelle angegeben');
         }
 
-        $mapper = new YFormMapper();
+        $mapper = new \KLXM\nocode\YFormMapper();
         
         try {
             $fields = $mapper->getTableFields($tableName);
             
-            return \rex_api_result::factory(true, [
+            return rex_api_result::factory(true, [
                 'fields' => $fields
             ]);
             
         } catch (\Exception $e) {
-            throw new \rex_api_exception($e->getMessage());
+            throw new rex_api_exception($e->getMessage());
         }
     }
 }
