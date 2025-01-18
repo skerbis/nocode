71a670718c70483d426d3fa155a2329ccc7a5365
Thomas Skerbis <thomas.skerbis@mac.com>
2025-01-18 23:18:36 +0100

    Create GetTemplateApi.php

---
+++ lib/api/GetTemplateApi.php
@@ -0,0 +1,59 @@
+<?php
+namespace KLXM\nocode;
+
+class GetTemplateApi extends \rex_api_function
+{
+    protected $published = true;
+
+    function execute()
+    {
+        $templateName = rex_request('template', 'string');
+        
+        if (!$templateName) {
+            throw new \rex_api_exception('Kein Template angegeben');
+        }
+
+        try {
+            $template = Template::getTemplate($templateName);
+            
+            if (!$template) {
+                throw new \rex_api_exception('Template nicht gefunden');
+            }
+
+            // Extrahiere die Felder und Optionen
+            $fields = [];
+            $options = [];
+
+            // Einfache Felder direkt übernehmen
+            foreach ($template['fields'] as $key => $field) {
+                if (!is_array($field) || !isset($field['type'])) {
+                    continue;
+                }
+                if ($field['type'] !== 'array') {
+                    $fields[$key] = $field;
+                    continue;
+                }
+                
+                // Bei Array-Feldern (z.B. items) die Unterfelder extrahieren
+                if (isset($field['fields'])) {
+                    foreach ($field['fields'] as $subKey => $subField) {
+                        $fields[$key . '.' . $subKey] = $subField;
+                    }
+                }
+            }
+
+            // Optionen extrahieren
+            if (isset($template['fields']['options']) && isset($template['fields']['options']['fields'])) {
+                $options = $template['fields']['options']['fields'];
+            }
+
+            return \rex_api_result::factory(true, [
+                'fields' => $fields,
+                'options' => $options
+            ]);
+            
+        } catch (\Exception $e) {
+            throw new \rex_api_exception($e->getMessage());
+        }
+    }
+}
